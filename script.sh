497928d90b9a33ba2d4f5d5b16cc595eb666ef0d
Roland Chao <rolandchao@nine-yi.com>
2022-12-17 00:32:49 +0800

    fix: 調整報告路徑

--- script.sh
+++ script.sh
@@ -1,4 +1,4 @@
 echo "執行${1}測試";
-pytest -k ${1} -n 5 --alluredir=./allure_report --clean-alluredir
+pytest -k ${1} -n 5 --alluredir=allure_report --clean-alluredir
 echo "打開allure報告";
-allure serve ./allure_report
+allure serve allure_report
